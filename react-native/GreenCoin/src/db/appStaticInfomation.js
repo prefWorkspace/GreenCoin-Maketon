723b20cda1722c3bcabd1d4bf37d85afa486099e
DESKTOP-V0G0BE3\sbj38 <sbj3834@naver.com>
2021-05-29 23:45:14 +0900

    스탭 카운터 테스트 가능하게 코드 셋팅 및 모듈 설치

--- react-native/GreenCoin/src/db/appStaticInfomation.js
+++ react-native/GreenCoin/src/db/appStaticInfomation.js
@@ -4,7 +4,7 @@ export default class appStaticInfomation {
 
     _area = null;
     _interest = null;
-
+    _step = false;
 
 
     _categorieslist = [];
--- react-native/GreenCoin/src/screens/content/ContentScreen.js
+++ react-native/GreenCoin/src/screens/content/ContentScreen.js
@@ -1,23 +1,61 @@
 import React, { Component,useState ,useEffect } from "react";
-import { Dimensions, View,BackHandler,ScrollView,TouchableOpacity,Image } from "react-native";
+import { Text,Dimensions, View,NativeEventEmitter,ScrollView,TouchableOpacity,Image } from "react-native";
 
 
 import MainTitle from '../../components/mains/main/MainTitle';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { WebView } from 'react-native-webview';
+import appStaticInfomation from '../../db/appStaticInfomation';
+import {
+  accelerometer,
+  gyroscope,
+  setUpdateIntervalForType,
+  SensorTypes
+} from "react-native-sensors";
+import { map, filter } from "rxjs/operators";
+
 
 const ContentScreen = () => {
- 
+
   const [webViewHeight,setWebViewHeight] = useState(100);
+  const [step,setStep] = useState(0);
+
+  setUpdateIntervalForType(SensorTypes.accelerometer, 100); // defaults to 100ms
+
+  const subscription = accelerometer
+  .pipe(map(({ x, y, z }) => x + y + z), filter(speed => speed > 20))
+  .subscribe(
+    speed => {
+      //console.log(`You moved your phone with ${speed}`)
+      stepUpdate();
+    },
+    error => {
+      console.log("The sensor is not available");
+    }
+  );
+
+  const stepUpdate = () => {
+    if(appStaticInfomation.getInstance()._step)
+      return;
+    appStaticInfomation.getInstance()._step = true;
+    console.log("아니");
+    setTimeout(()=>{
+        setStep(e => e + 1);
+        appStaticInfomation.getInstance()._step = false;
+    },1000);
+  }
 
   const onWebViewMessage = (event) => {
       setWebViewHeight(Number(event.nativeEvent.data));
   }
+
+
 //172.28.5.10
   return (
     <View style={styles.container}>
       <MainTitle/>
       <View>
+        <Text>step : {step}</Text>
         <ScrollView  contentContainerStyle={{flexGrow: 1, height : webViewHeight}}>
           <WebView
               // source={{ uri: `http://172.28.5.10:3000` }}
